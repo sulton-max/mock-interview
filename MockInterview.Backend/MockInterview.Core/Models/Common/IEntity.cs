9af6c703f2eadf981ab596cd4bde971731932ce6
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2022-12-17 16:15:52 +0500

    #m1-4 Created Core Project

---
+++ MockInterview.Backend/MockInterview.Core/Models/Common/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace MockInterview.Core.Models.Common;
+
+/// <summary>
+/// Common Interface for Database Entities 
+/// </summary>
+public interface IEntity
+{
+    /// <summary>
+    /// Gets or sets Primary Key 
+    /// </summary>
+    long Id { get; set; }
+}
