f2aa768f8c2e7d86951b73b3f2353012be7df954
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2022-12-17 16:29:05 +0500

    #m1-3 Created BLL Project

---
+++ MockInterview.Backend/MockInterview.BLL/Models/Common/IEntityDto.cs
@@ -0,0 +1,12 @@
+namespace MockInterview.BLL.Models.Common;
+
+/// <summary>
+/// Common Interface for entity dto model
+/// </summary>
+public interface IEntityDto
+{
+    /// <summary>
+    /// Entity model Primary Key
+    /// </summary>
+    long Id { get; set; }
+}
---
+++ MockInterview.Backend/MockInterview.BLL/Services/EntityServices/Interfaces/IEntityServiceBase.cs
@@ -0,0 +1,56 @@
+﻿using MockInterview.Core.Models.Common;
+
+namespace MockInterview.BLL.Services.EntityServices.Interfaces;
+
+/// <summary>
+/// Defines common business logic to manipulate entities
+/// </summary>
+/// <typeparam name="TEntity">Entity type</typeparam>
+public interface IEntityServiceBase<TEntity> where TEntity : class, IEntity
+{
+    /// <summary>
+    /// Gets the entity that has the required id
+    /// </summary>
+    /// <param name="id">Id of the entity being queried</param>
+    /// <returns>Entity if found, or null</returns>
+    Task<TEntity?> GetByIdAsync(long id);
+
+    /// <summary>
+    /// Gets entities that matches given array of ids
+    /// </summary>
+    /// <param name="ids"></param>
+    /// <returns>Array of entities or empty array</returns>
+    Task<IEnumerable<TEntity>> GetByIdsAsync(ICollection<long> ids);
+
+    /// <summary>
+    /// Creates entry on Context scope or on Database
+    /// </summary>
+    /// <param name="entity">Entity to create</param>
+    /// <param name="saveChanges">Determines whether to commit changes to Database</param>
+    /// <returns>Result of entity creation</returns>
+    Task<TEntity?> CreateAsync(TEntity entity, bool saveChanges = true);
+
+    /// <summary>
+    /// Updates entity on context scope or on Database
+    /// </summary>
+    /// <param name="entity">Entity to update</param>
+    /// <param name="saveChanges">Determines whether to commit changes to Database</param>
+    /// <returns>Result of entity update</returns>
+    Task<bool> UpdateAsync(long id, TEntity entity, bool saveChanges = true);
+
+    /// <summary>
+    /// Deletes entity on context scope or on Database with its Id
+    /// </summary>
+    /// <param name="id">Id of Entity to delete</param>
+    /// <param name="saveChanges">Determines whether to commit changes to Database</param>
+    /// <returns>Result of entity deletion</returns>
+    Task<bool> DeleteByIdAsync(long id, bool saveChanges = true);
+
+    /// <summary>
+    /// Deletes entity on context scope or on Database
+    /// </summary>
+    /// <param name="entity">Entity to delete</param>
+    /// <param name="saveChanges">Determines whether to commit changes to Database</param>
+    /// <returns>Result of entity deletion</returns>
+    Task<bool> DeleteAsync(TEntity entity, bool saveChanges = true);
+}
