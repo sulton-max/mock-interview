f321798a9c2453e58cce761b02eacb712a61ff0c
tohirbek009 <odilovtohirbek009@gmail.com>
2022-12-18 11:09:23 +0500

    #m1-9 Create Dashboard page

--- MockInterview.Frontend/src/router/index.ts
+++ MockInterview.Frontend/src/router/index.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
+import {DashboardChildren} from "@/router/dashboard/dashboardChildren";
+import Dashboard from "@/views/Dashboard/Dashboard.vue";
 import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
@@ -17,7 +19,14 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/dashboard',
+    redirect: '/dashboard/interviewers',
+    name: "Dashboard",
+    component: Dashboard,
+    children: DashboardChildren,
+  },
 ]
 
 const router = new VueRouter({
